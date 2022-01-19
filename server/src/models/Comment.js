af72d8d1240d00247f97e84824d4e4a4c86939ea
mishostef <mishostef@abv.bg>
2022-01-19 14:58:53 +0200

    removed Files

--- server/src/models/Comment.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Comment = void 0;
-var mongoose_1 = require("mongoose");
-var schema = new mongoose_1.Schema({
-    text: { type: String, required: true },
-    authorId: { type: mongoose_1.Schema.Types.ObjectId, ref: 'User', "default": [] }
-});
-exports.Comment = mongoose_1.model('Comment', schema);
