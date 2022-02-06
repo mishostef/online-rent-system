fc089e293538ac2983336849018d34d5228de208
mishostef <mishostef@abv.bg>
2022-02-06 17:03:31 +0200

    added wifi option

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -1,7 +1,7 @@
 import * as cookieParser from "cookie-parser";
 import * as express from "express";
 import { Request, Response } from "express";
-import { auth as authMiddleware } from '../middlewares/auth'
+import { auth as authMiddleware } from '../middlewares/auth';
 import * as cors from 'cors';
 import * as os from 'os';
 
@@ -12,8 +12,9 @@ export const expressConfig = (app: express.Application) => {
     app.use(authMiddleware());
 
     var networkInterfaces = os.networkInterfaces();
-
-    const myIP = (networkInterfaces['Local Area Connection'][1]['address']);
+    console.dir(networkInterfaces);
+    const myIP = networkInterfaces['Local Area Connection'] ? (networkInterfaces['Local Area Connection'][1]['address']) :
+        networkInterfaces['Wi-Fi'][1]['address'];
     console.log(myIP);
     app.options(`http://${myIP.toString()}:3000`, cors());
 
@@ -23,11 +24,11 @@ export const expressConfig = (app: express.Application) => {
         allowedHeaders: '*',
         optionsSuccessStatus: 200,// some legacy browsers (IE11, various SmartTVs) choke on 204
         credentials: true
-    }
+    };
 
-    app.use(cors(corsOptions));    
+    app.use(cors(corsOptions));
 
-    app.use((req: Request, res: Response, next) => {       
+    app.use((req: Request, res: Response, next) => {
         console.log(">>>", req.method, req.url, `headers: ${JSON.stringify(req.headers['authorization'])}`);
         if (req['user']) {
             console.log(`known user`, req['user'].email);
